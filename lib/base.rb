77e2fe149439623bd709d702a9f635e2cb37c287
Robert Harężlak <harezlak.robert7@gmail.com>
2017-06-13 11:14:18 +0200

    include remaining figures & apply expected output

--- lib/base.rb
+++ lib/base.rb
@@ -6,23 +6,23 @@ class Base
     position_validation
     start_position_on_board?
   end
-  
+
   def position_validation
-    raise ArgumentError.new("ArgumentError: Pass x_axis value and y_values (example: 'd4')") if @position.length > 2
+    raise ArgumentError, "ArgumentError: Pass x_axis value and y_values (example: 'd4')" if @position.length > 2
   end
-  
+
   def start_position_on_board?
-    raise ArgumentError.new("Passed values are not included on board - Pass something else") if !(on_board?(x_axis, y_axis))
+    raise ArgumentError, 'Passed values are not included on board - Pass something else' unless on_board?(x_axis, y_axis)
   end
 
   def x_axis
-    @position[0].upcase.ord - 64  
+    @position[0].upcase.ord - 64
   end
 
   def y_axis
     @position[1].to_i
   end
-  
+
   def on_board?(x, y)
     (1..8).cover?(x) && (1..8).cover?(y)
   end
@@ -34,12 +34,12 @@ class Base
   def moves_on_board
     possible_moves.select { |move| on_board?(move.first, move.last) }
   end
-  
+
   def exec
-    moves_on_board.map { |x, y| [(x + 64).chr, y].join('') }.each { |i| puts i }
+    moves_on_board.map { |x, y| [(x + 64).chr, y].join('') }
   end
-  
+
   def vectors
-    raise NotImplementedError.new("Please pass vectors from external class")
+    raise NotImplementedError, 'Please pass vectors from external class'
   end
 end
--- run.rb
+++ run.rb
@@ -1,6 +1,10 @@
 require_relative 'lib/bishop'
+require_relative 'lib/king'
+require_relative 'lib/queen'
+require_relative 'lib/rook'
+require_relative 'lib/knight'
 require_relative 'lib/base'
 
 # use in this way - Bishop.new('b4').exec
 
-Bishop.new('d1').exec
+p Bishop.new('b4').exec
