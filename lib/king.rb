c9e4e3bc503a3cc3b862c9230475ba0575e33b3e
Robert Harężlak <harezlak.robert7@gmail.com>
2017-06-13 11:33:43 +0200

    using .freeze on const is good practice

--- lib/king.rb
+++ lib/king.rb
@@ -1,7 +1,7 @@
 require_relative 'base'
 
 class King < Base
-  VECTORS = [[1, 0], [-1, 0], [0, 1], [0 , -1], [1, 1], [1, -1], [-1, 1], [-1, -1]]
+  VECTORS = [[1, 0], [-1, 0], [0, 1], [0 , -1], [1, 1], [1, -1], [-1, 1], [-1, -1]].freeze
 
   def vectors
     VECTORS
--- lib/knight.rb
+++ lib/knight.rb
@@ -1,7 +1,7 @@
 require_relative 'base'
 class Knight < Base
 
-  VECTORS = [[2, 1], [2, -1], [-2, 1], [-2, -1], [-1, 2], [-1, -2], [1, -2], [1, 2]]
+  VECTORS = [[2, 1], [2, -1], [-2, 1], [-2, -1], [-1, 2], [-1, -2], [1, -2], [1, 2]].freeze
 
   def vectors
     VECTORS
